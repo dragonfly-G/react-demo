d655a340f00bf2f9071083ac9cc9e993432d5f1f
dragonfly-G <911025lf>
2019-03-14 14:06:00 +0800

    添加layout

---
+++ src/components/Sliderbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Sliderbar extends React.Component {
+  render() {
+    return (
+      <div>
+        开启项目
+      </div>
+    );
+  }
+}
+
+export default Sliderbar;
---
+++ src/components/Topbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Topbar extends React.Component {
+  render() {
+    return (
+      <div>
+        开启项目
+      </div>
+    );
+  }
+}
+
+export default Topbar;
